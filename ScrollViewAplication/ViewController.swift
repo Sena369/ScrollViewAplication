be29d90e2a0fce467406ef3e69d89e4146212493
Sena <senana0116@icloud.com>
2022-03-03 17:17:46 +0900

    レイアウト済み

---
+++ ScrollViewAplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewAplication
+//
+//  Created by 澤田世那 on 2022/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var container: UIView!
+    
+}
+
