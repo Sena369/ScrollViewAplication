8d2059e9c23bd5be63f1ffc36f05ca80531a1ba4
Sena <senana0116@icloud.com>
2022-03-04 15:57:29 +0900

    初期化

---
+++ ScrollViewAplication/Container.swift
@@ -0,0 +1,20 @@
+//
+//  Container.swift
+//  ScrollViewAplication
+//
+//  Created by 澤田世那 on 2022/03/04.
+//
+
+import UIKit
+
+class Container: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
